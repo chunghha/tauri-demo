b323e8e20cc8b9880eed857f41eae662ebf2f0ce
Chris Ha <chunghha@users.noreply.github.com>
2022-06-11 09:43:25 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <div data-theme="rosepine">
+    <div class="grid place-items-center h-screen">
+      <div>
+        <img alt="Vue logo" src="./assets/logo.png" />
+      </div>
+      <div>
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      </div>
+    </div>
+  </div>
+</template>

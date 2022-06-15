663a51de77e0cfcc47b4c31e9565773ffe903846
Chris Ha <chunghha@users.noreply.github.com>
2022-06-14 20:54:10 -0500

    chore: @vueuse/motion

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import { MotionPlugin } from '@vueuse/motion';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import './index.css';
 import { router } from './router/router';
 
-createApp(App).use(createPinia()).use(router).mount('#app');
+createApp(App).use(createPinia()).use(router).use(MotionPlugin).mount('#app');
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -15,12 +15,21 @@ const xxxl = breakpoints['2xl'];
 <template>
   <h1 class="text-center text-warning">about page</h1>
   <div class="divider"></div>
-  <div class="btn-group flex justify-center">
-    <button class="btn" :class="{ 'btn-secondary': sm }">sm</button>
-    <button class="btn" :class="{ 'btn-secondary': md }">md</button>
-    <button class="btn" :class="{ 'btn-secondary': lg }">lg</button>
-    <button class="btn" :class="{ 'btn-secondary': xl }">xl</button>
-    <button class="btn" :class="{ 'btn-secondary': xxl }">xxl</button>
-    <button class="btn" :class="{ 'btn-secondary': xxxl }">xxxl</button>
+  <div v-motion :initial="{ y: 300, opacity: 0, scale: 1 }" :enter="{
+    y: 0, opacity: 1, transition: {
+      y: { delay: 700, }, opacity: {
+        duration: 500,
+      },
+    },
+  }" :tapped="{ scale: 1.2, }">
+    <div class="btn-group flex justify-center">
+      <button class="btn" :class="{ 'btn-secondary': sm }">sm</button>
+      <button class="btn" :class="{ 'btn-secondary': md }">md</button>
+      <button class="btn" :class="{ 'btn-secondary': lg }">lg</button>
+      <button class="btn" :class="{ 'btn-secondary': xl }">xl</button>
+      <button class="btn" :class="{ 'btn-secondary': xxl }">xxl</button>
+      <button class="btn" :class="{ 'btn-secondary': xxxl }">xxxl</button>
+    </div>
   </div>
+
 </template>
